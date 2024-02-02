ee11fa2611b366e1e094e3b7a83f7abf9ed2cfa3
Agatha V. Lovelace <agatha@technogothic.net>
2024-02-02 18:02:00 +0100

    Bump version to 4.3.0-alpha.1 to remove vulnerability warning

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -13,7 +13,7 @@ module Mastodon
     end
 
     def patch
-      0
+      1
     end
 
     def default_prerelease
@@ -25,7 +25,7 @@ module Mastodon
     end
 
     def build_metadata
-      ['glitch', ENV.fetch('MASTODON_VERSION_METADATA', nil)].compact_blank.join('.')
+      ['glitch', ENV.fetch('MASTODON_VERSION_METADATA', nil)].compact_blank.join('+')
     end
 
     def to_a
